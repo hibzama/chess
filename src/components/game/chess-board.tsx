41b81ef791fbea4cec2b16e1067cd9b22ac414d7
Qrex Academy <jdilshan306@gmail.com>
2025-07-23 17:06:33 +0000

    Make all the pieces in the chess game like the screenshot I sent you....

--- src/components/game/chess-board.tsx
+++ src/components/game/chess-board.tsx
@@ -25,12 +25,12 @@ const boardThemes: BoardTheme[] = [
 const getPieceIcon = (type: string, color: string) => {
     const style = { fill: color, stroke: color === '#0f172a' || color === '#18181b' ? '#f8fafc' : '#0f172a' , strokeWidth: 1.5, strokeLinejoin: 'round' } as React.CSSProperties;
     switch (type.toLowerCase()) {
-        case 'p': return <g {...style}><path d="M22.5 9c-2.21 0-4 1.79-4 4 0 .89.29 1.71.78 2.38-1.95 1.12-3.28 3.2-3.28 5.62h9c0-2.42-1.33-4.5-3.28-5.62.49-.67.78-1.49.78-2.38 0-2.21-1.79-4-4-4zM12 31h1.5v-2.5h9V31H24v-1.5h-4.5V31z" /></g>;
-        case 'r': return <g {...style}><path d="M13 31h19v-3H13zM14 11V9h17v2h-3v2h-3v-2h-5v2h-3V11zM14 28h17v-9H14z" /></g>;
-        case 'n': return <g {...style}><path d="M19.5 7.5c-1.66 0-3 1.34-3 3v9h1.5v-4.5c1.2-1.8 3.48-3 6-3s4.8 1.2 6 3V18h1.5v-6c0-4.14-3.36-7.5-7.5-7.5zm-3 15h12v1.5h-12zm-3 3h18v1.5h-18zm-1.5 3h21v3h-21z" /></g>;
-        case 'b': return <g {...style}><path d="M18 9c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zM12 31h21v-3H12zm2.5-3.5c-2.5 0-4.5 2-4.5 4.5h14c0-2.5-2-4.5-4.5-4.5zM19.5 14.5c-3.04 0-5.5 2.46-5.5 5.5s2.46 5.5 5.5 5.5 5.5-2.46 5.5-5.5-2.46-5.5-5.5-5.5z" /></g>;
-        case 'q': return <g {...style}><path d="M11 12l2.5-3 2.5 3h-5zm11 0l2.5-3 2.5 3h-5zM16 14h13v2H16zm-5 17h23v-3H11zM12.5 28h20v-9h-20zm-1.5-12h23v2h-23z" /></g>;
-        case 'k': return <g {...style}><path d="M22.5 7.5l-3 3h6l-3-3zM15 31h15v-3H15zm-1.5-3.5c-2.48 0-4.5 2.02-4.5 4.5h18c0-2.48-2.02-4.5-4.5-4.5zM16 12h13v2H16zm-2 2h17v12H14z" /></g>;
+        case 'p': return <g {...style}><path d="M22.5 9c-2.21 0-4 1.79-4 4 0 .89.29 1.71.78 2.38C17.33 16.5 16 18.58 16 21v-1h13v1c0-2.42-1.33-4.5-3.28-5.62.49-.67.78-1.49.78-2.38 0-2.21-1.79-4-4-4zM12 31h21v-3H12v3z" /></g>;
+        case 'r': return <g {...style}><path d="M13 31h19v-3H13v3zm1-4h17v-8H14v8zM11 9h23v5h-3v2h-3v-2h-5v2h-3v-2h-3V9z" /></g>;
+        case 'n': return <g {...style}><path d="M22.5 31h-9V29.5h9V31zm-3-1.5a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0zm-1.5-12c-3.44.64-6.44 3.69-7.5 7.5-1.5 5.5 2 9.5 7.5 9.5h3V15.5H18c-2.5 0-2.5-2.5-2.5-2.5s3-1.5 6-1.5c3.5 0 5.5 2.5 5.5 2.5v14h1.5v-13c0-4.67-3.33-8-8-8z" /></g>;
+        case 'b': return <g {...style}><path d="M18.5 31h8v-1.5h-8V31zM15 28.5a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0zM12 28.5c0-4.67 3.33-8 8-8s8 3.33 8 8c0 1.15-.22 2.25-.62 3.25H12.62c-.4-1-.62-2.1-.62-3.25zM19.5 9a3 3 0 1 1 6 0 3 3 0 0 1-6 0z" /></g>;
+        case 'q': return <g {...style}><path d="M12.5 31h20v-3h-20v3zM10.5 12c0-3.31 2.69-6 6-6s6 2.69 6 6c0 1.54-.58 2.94-1.5 4h-9c-.92-1.06-1.5-2.46-1.5-4zM10 28h25v-3H10v3z" /></g>;
+        case 'k': return <g {...style}><path d="M22.5 31h-9V29.5h9V31zm-4.5-1.5a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0zM12 28.5c0-4.67 3.33-8 8-8s8 3.33 8 8v3h-16v-3zM22.5 6h-3v3h-3v3h3v-3h3V6z" /></g>;
         default: return null;
     }
 }
