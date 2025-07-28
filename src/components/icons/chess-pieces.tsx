78a5ff62a50011d26c4d99b0cb97c6ab0f4c0f36
Qrex Academy <jdilshan306@gmail.com>
2025-07-28 09:49:04 +0000

    still pawans / queen / king dising not good

--- src/components/icons/chess-pieces.tsx
+++ src/components/icons/chess-pieces.tsx
@@ -7,106 +7,90 @@ const pieceStyles = {
     strokeWidth: 1.5,
 };
 
-const whitePieceStyles = { ...pieceStyles, stroke: "#c7c7c7" };
-const blackPieceStyles = { ...pieceStyles, stroke: "#1c1c1c" };
+const whitePieceStyles = { ...pieceStyles, stroke: "#000" };
+const blackPieceStyles = { ...pieceStyles, stroke: "#fff" };
 
 export const WhitePawn = (props: SVGProps<SVGSVGElement>) => (
     <svg viewBox="0 0 45 45" {...props}>
         <g {...whitePieceStyles} style={{ opacity: 1, fill: "#fff", fillOpacity: 1, fillRule: "evenodd", stroke: "#000", strokeWidth: 1.5, strokeLinecap: "round", strokeLinejoin: "round", strokeMiterlimit: 4, strokeDasharray: "none", strokeOpacity: 1, }}>
-            <path d="M 22.5,11.63 C 22.5,11.63 22.5,11.63 22.5,11.63 C 22.5,11.63 22.5,11.63 22.5,11.63 C 20.12,11.63 18.25,13.5 18.25,15.88 C 18.25,18.25 20.12,20.13 22.5,20.13 C 24.88,20.13 26.75,18.25 26.75,15.88 C 26.75,13.5 24.88,11.63 22.5,11.63 Z M 22.5,22 C 19.5,22 19,23.5 19,23.5 L 19,26 C 19,26 20.5,26 22.5,26 C 24.5,26 26,26 26,26 L 26,23.5 C 26,23.5 25.5,22 22.5,22 Z M 20,28 L 20,31.5 C 20,31.5 20,31.5 20,31.5 C 20,31.5 20,31.5 20,31.5 C 20,32.33 20.67,33 21.5,33 L 23.5,33 C 24.33,33 25,32.33 25,31.5 L 25,28 Z M 17.5,34 L 17.5,35.5 L 27.5,35.5 L 27.5,34 L 17.5,34 Z" style={{ strokeLinecap: "butt" }} />
+           <path d="M22.5 9c-2.21 0-4 1.79-4 4 0 .89.29 1.71.78 2.38C17.33 16.5 16 18.58 16 21v-1h13v1c0-2.42-1.33-4.5-3.28-5.62.49-.67.78-1.49.78-2.38 0-2.21-1.79-4-4-4zM12 31h21v-3H12v3z" />
         </g>
     </svg>
 );
 export const BlackPawn = (props: SVGProps<SVGSVGElement>) => (
     <svg viewBox="0 0 45 45" {...props}>
         <g {...blackPieceStyles} style={{ opacity: 1, fill: "#000", fillOpacity: 1, fillRule: "evenodd", stroke: "#fff", strokeWidth: 1.5, strokeLinecap: "round", strokeLinejoin: "round", strokeMiterlimit: 4, strokeDasharray: "none", strokeOpacity: 1, }}>
-            <path d="M 22.5,11.63 C 22.5,11.63 22.5,11.63 22.5,11.63 C 22.5,11.63 22.5,11.63 22.5,11.63 C 20.12,11.63 18.25,13.5 18.25,15.88 C 18.25,18.25 20.12,20.13 22.5,20.13 C 24.88,20.13 26.75,18.25 26.75,15.88 C 26.75,13.5 24.88,11.63 22.5,11.63 Z M 22.5,22 C 19.5,22 19,23.5 19,23.5 L 19,26 C 19,26 20.5,26 22.5,26 C 24.5,26 26,26 26,26 L 26,23.5 C 26,23.5 25.5,22 22.5,22 Z M 20,28 L 20,31.5 C 20,31.5 20,31.5 20,31.5 C 20,31.5 20,31.5 20,31.5 C 20,32.33 20.67,33 21.5,33 L 23.5,33 C 24.33,33 25,32.33 25,31.5 L 25,28 Z M 17.5,34 L 17.5,35.5 L 27.5,35.5 L 27.5,34 L 17.5,34 Z" style={{ strokeLinecap: "butt" }} />
+             <path d="M22.5 9c-2.21 0-4 1.79-4 4 0 .89.29 1.71.78 2.38C17.33 16.5 16 18.58 16 21v-1h13v1c0-2.42-1.33-4.5-3.28-5.62.49-.67.78-1.49.78-2.38 0-2.21-1.79-4-4-4zM12 31h21v-3H12v3z" />
         </g>
     </svg>
 );
 export const WhiteRook = (props: SVGProps<SVGSVGElement>) => (
     <svg viewBox="0 0 45 45" {...props}>
         <g {...whitePieceStyles} style={{ opacity: 1, fill: "#fff", fillOpacity: 1, fillRule: "evenodd", stroke: "#000", strokeWidth: 1.5, strokeLinecap: "round", strokeLinejoin: "round", strokeMiterlimit: 4, strokeDasharray: "none", strokeOpacity: 1, }}>
-            <path d="M 9,39 L 36,39 L 36,36 L 9,36 L 9,39 Z M 12,36 L 12,32 L 33,32 L 33,36 L 12,36 Z M 11,14 L 11,9 L 15,9 L 15,11 L 20,11 L 20,9 L 25,9 L 25,11 L 30,11 L 30,9 L 34,9 L 34,14 L 11,14 Z" style={{ strokeLinecap: "butt" }} />
-            <path d="M 34,14 L 31,17 L 14,17 L 11,14" />
-            <path d="M 31,17 L 31,29.5 L 14,29.5 L 14,17" style={{ strokeLinecap: "butt", strokeLinejoin: "miter" }} />
-            <path d="M 31,29.5 L 32.5,32 L 12.5,32 L 14,29.5" />
-            <path d="M 11,14 L 34,14" style={{ fill: "none", stroke: "#000", strokeLinejoin: "miter" }} />
+           <path d="M13 31h19v-3H13v3zm1-4h17v-8H14v8zM11 9h23v5h-3v2h-3v-2h-5v2h-3v-2h-3V9z" />
         </g>
     </svg>
 );
 export const BlackRook = (props: SVGProps<SVGSVGElement>) => (
     <svg viewBox="0 0 45 45" {...props}>
         <g {...blackPieceStyles} style={{ opacity: 1, fill: "#000", fillOpacity: 1, fillRule: "evenodd", stroke: "#fff", strokeWidth: 1.5, strokeLinecap: "round", strokeLinejoin: "round", strokeMiterlimit: 4, strokeDasharray: "none", strokeOpacity: 1, }}>
-            <path d="M 9,39 L 36,39 L 36,36 L 9,36 L 9,39 Z M 12,36 L 12,32 L 33,32 L 33,36 L 12,36 Z M 11,14 L 11,9 L 15,9 L 15,11 L 20,11 L 20,9 L 25,9 L 25,11 L 30,11 L 30,9 L 34,9 L 34,14 L 11,14 Z" style={{ strokeLinecap: "butt" }} />
-            <path d="M 34,14 L 31,17 L 14,17 L 11,14" />
-            <path d="M 31,17 L 31,29.5 L 14,29.5 L 14,17" style={{ strokeLinecap: "butt", strokeLinejoin: "miter" }} />
-            <path d="M 31,29.5 L 32.5,32 L 12.5,32 L 14,29.5" />
-            <path d="M 11,14 L 34,14" style={{ fill: "none", stroke: "#fff", strokeLinejoin: "miter" }} />
+            <path d="M13 31h19v-3H13v3zm1-4h17v-8H14v8zM11 9h23v5h-3v2h-3v-2h-5v2h-3v-2h-3V9z" />
         </g>
     </svg>
 );
 export const WhiteKnight = (props: SVGProps<SVGSVGElement>) => (
     <svg viewBox="0 0 45 45" {...props}>
         <g {...whitePieceStyles} style={{ opacity: 1, fill: "#fff", fillOpacity: 1, fillRule: "evenodd", stroke: "#000", strokeWidth: 1.5, strokeLinecap: "round", strokeLinejoin: "round", strokeMiterlimit: 4, strokeDasharray: "none", strokeOpacity: 1, }}>
-            <path d="M 22,10 C 32.5,10 31.5,18 31.5,18 C 31.5,24 28,29 28,29 L 15.5,29 C 15.5,29 13.5,24.5 13.5,24.5 C 13.5,24.5 13.5,19.5 13.5,19.5 C 13.5,19.5 14,16.5 14,16.5 C 14,16.5 11.5,14.5 11.5,14.5 C 11.5,14.5 10.5,12 10.5,12 C 10.5,12 12.5,10 12.5,10 C 12.5,10 15,11.5 15,11.5 C 15,11.5 16,10 22,10 Z M 15.5,29 L 15.5,32 L 28,32 L 28,29 M 12.5,32 L 31.5,32 L 31.5,39 L 12.5,39 L 12.5,32 Z" style={{ strokeLinecap: "butt" }} />
+            <path d="M22.5 31h-9V29.5h9V31zm-3-1.5a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0zm-1.5-12c-3.44.64-6.44 3.69-7.5 7.5-1.5 5.5 2 9.5 7.5 9.5h3V15.5H18c-2.5 0-2.5-2.5-2.5-2.5s3-1.5 6-1.5c3.5 0 5.5 2.5 5.5 2.5v14h1.5v-13c0-4.67-3.33-8-8-8z" />
         </g>
     </svg>
 );
 export const BlackKnight = (props: SVGProps<SVGSVGElement>) => (
     <svg viewBox="0 0 45 45" {...props}>
         <g {...blackPieceStyles} style={{ opacity: 1, fill: "#000", fillOpacity: 1, fillRule: "evenodd", stroke: "#fff", strokeWidth: 1.5, strokeLinecap: "round", strokeLinejoin: "round", strokeMiterlimit: 4, strokeDasharray: "none", strokeOpacity: 1, }}>
-            <path d="M 22,10 C 32.5,10 31.5,18 31.5,18 C 31.5,24 28,29 28,29 L 15.5,29 C 15.5,29 13.5,24.5 13.5,24.5 C 13.5,24.5 13.5,19.5 13.5,19.5 C 13.5,19.5 14,16.5 14,16.5 C 14,16.5 11.5,14.5 11.5,14.5 C 11.5,14.5 10.5,12 10.5,12 C 10.5,12 12.5,10 12.5,10 C 12.5,10 15,11.5 15,11.5 C 15,11.5 16,10 22,10 Z M 15.5,29 L 15.5,32 L 28,32 L 28,29 M 12.5,32 L 31.5,32 L 31.5,39 L 12.5,39 L 12.5,32 Z" style={{ strokeLinecap: "butt" }} />
+           <path d="M22.5 31h-9V29.5h9V31zm-3-1.5a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0zm-1.5-12c-3.44.64-6.44 3.69-7.5 7.5-1.5 5.5 2 9.5 7.5 9.5h3V15.5H18c-2.5 0-2.5-2.5-2.5-2.5s3-1.5 6-1.5c3.5 0 5.5 2.5 5.5 2.5v14h1.5v-13c0-4.67-3.33-8-8-8z" />
         </g>
     </svg>
 );
 export const WhiteBishop = (props: SVGProps<SVGSVGElement>) => (
     <svg viewBox="0 0 45 45" {...props}>
         <g {...whitePieceStyles} style={{ opacity: 1, fill: "#fff", fillOpacity: 1, fillRule: "evenodd", stroke: "#000", strokeWidth: 1.5, strokeLinecap: "round", strokeLinejoin: "round", strokeMiterlimit: 4, strokeDasharray: "none", strokeOpacity: 1, }}>
-            <path d="M 9,36 C 12.39,35.03 19.11,36.43 22.5,34 C 25.89,36.43 32.61,35.03 36,36 C 36,36 37.65,36.54 39,38 C 39,38 39,38 39,38 C 39,38 39,38 39,38 L 6,38 C 7.35,36.54 9,36 9,36 Z M 22.5,34 C 22.5,34 22.52,34.17 22.5,34.5 C 22.48,34.17 22.5,34 22.5,34 C 22.5,34 22.5,34 22.5,34 Z M 15,32 C 15,32 15,29.5 15,29.5 C 15,29.5 15,27 15,27 C 15,27 17.5,24.5 17.5,24.5 L 27.5,24.5 C 27.5,24.5 30,27 30,27 C 30,27 30,29.5 30,29.5 C 30,29.5 30,32 30,32 L 15,32 Z M 25,23 C 25,23 25.5,20 25.5,18.5 C 25.5,17 24.5,15.5 22.5,15.5 C 20.5,15.5 19.5,17 19.5,18.5 C 19.5,20 20,23 20,23 L 25,23 Z M 22.5,15.5 L 22.5,12.5 L 22.5,15.5 Z M 22.5,12.5 C 21.5,12.5 20.5,11.5 20.5,10.5 C 20.5,9.5 21.5,8.5 22.5,8.5 C 23.5,8.5 24.5,9.5 24.5,10.5 C 24.5,11.5 23.5,12.5 22.5,12.5 Z M 22.5,12.5 L 22.5,15.5" style={{ strokeLinecap: "butt" }} />
+           <path d="M18.5 31h8v-1.5h-8V31zM15 28.5a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0zM12 28.5c0-4.67 3.33-8 8-8s8 3.33 8 8c0 1.15-.22 2.25-.62 3.25H12.62c-.4-1-.62-2.1-.62-3.25zM19.5 9a3 3 0 1 1 6 0 3 3 0 0 1-6 0z" />
         </g>
     </svg>
 );
 export const BlackBishop = (props: SVGProps<SVGSVGElement>) => (
     <svg viewBox="0 0 45 45" {...props}>
         <g {...blackPieceStyles} style={{ opacity: 1, fill: "#000", fillOpacity: 1, fillRule: "evenodd", stroke: "#fff", strokeWidth: 1.5, strokeLinecap: "round", strokeLinejoin: "round", strokeMiterlimit: 4, strokeDasharray: "none", strokeOpacity: 1, }}>
-            <path d="M 9,36 C 12.39,35.03 19.11,36.43 22.5,34 C 25.89,36.43 32.61,35.03 36,36 C 36,36 37.65,36.54 39,38 C 39,38 39,38 39,38 C 39,38 39,38 39,38 L 6,38 C 7.35,36.54 9,36 9,36 Z M 22.5,34 C 22.5,34 22.52,34.17 22.5,34.5 C 22.48,34.17 22.5,34 22.5,34 C 22.5,34 22.5,34 22.5,34 Z M 15,32 C 15,32 15,29.5 15,29.5 C 15,29.5 15,27 15,27 C 15,27 17.5,24.5 17.5,24.5 L 27.5,24.5 C 27.5,24.5 30,27 30,27 C 30,27 30,29.5 30,29.5 C 30,29.5 30,32 30,32 L 15,32 Z M 25,23 C 25,23 25.5,20 25.5,18.5 C 25.5,17 24.5,15.5 22.5,15.5 C 20.5,15.5 19.5,17 19.5,18.5 C 19.5,20 20,23 20,23 L 25,23 Z M 22.5,15.5 L 22.5,12.5 L 22.5,15.5 Z M 22.5,12.5 C 21.5,12.5 20.5,11.5 20.5,10.5 C 20.5,9.5 21.5,8.5 22.5,8.5 C 23.5,8.5 24.5,9.5 24.5,10.5 C 24.5,11.5 23.5,12.5 22.5,12.5 Z M 22.5,12.5 L 22.5,15.5" style={{ strokeLinecap: "butt" }} />
+            <path d="M18.5 31h8v-1.5h-8V31zM15 28.5a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0zM12 28.5c0-4.67 3.33-8 8-8s8 3.33 8 8c0 1.15-.22 2.25-.62 3.25H12.62c-.4-1-.62-2.1-.62-3.25zM19.5 9a3 3 0 1 1 6 0 3 3 0 0 1-6 0z" />
         </g>
     </svg>
 );
 export const WhiteQueen = (props: SVGProps<SVGSVGElement>) => (
     <svg viewBox="0 0 45 45" {...props}>
         <g {...whitePieceStyles} style={{ opacity: 1, fill: "#fff", fillOpacity: 1, fillRule: "evenodd", stroke: "#000", strokeWidth: 1.5, strokeLinecap: "round", strokeLinejoin: "round", strokeMiterlimit: 4, strokeDasharray: "none", strokeOpacity: 1, }}>
-            <path d="M 8,12 L 37,12 L 37,26 L 8,26 L 8,12 Z M 8,26 L 14,29.5 L 31,29.5 L 37,26 M 14,29.5 L 14,31.5 L 31,31.5 L 31,29.5 M 14,31.5 C 14,31.5 14,34 14,34 C 14,34 11,34 11,34 C 11,34 11,36 11,36 C 11,36 12.5,38 12.5,38 L 32.5,38 C 32.5,38 34,36 34,36 C 34,36 34,34 34,34 C 34,34 31,34 31,34 C 31,34 31,31.5 31,31.5 M 12.5,38 L 12.5,40 L 32.5,40 L 32.5,38" style={{ strokeLinecap: "butt" }} />
-            <path d="M 8,12 C 8,12 8.5,4.5 14,4.5 C 19.5,4.5 22.5,7 22.5,7 C 22.5,7 25.5,4.5 31,4.5 C 36.5,4.5 37,12 37,12" style={{ strokeLinecap: "butt" }} />
+            <path d="M12.5 31h20v-3h-20v3zM10.5 12c0-3.31 2.69-6 6-6s6 2.69 6 6c0 1.54-.58 2.94-1.5 4h-9c-.92-1.06-1.5-2.46-1.5-4zM10 28h25v-3H10v3z" />
         </g>
     </svg>
 );
 export const BlackQueen = (props: SVGProps<SVGSVGElement>) => (
     <svg viewBox="0 0 45 45" {...props}>
         <g {...blackPieceStyles} style={{ opacity: 1, fill: "#000", fillOpacity: 1, fillRule: "evenodd", stroke: "#fff", strokeWidth: 1.5, strokeLinecap: "round", strokeLinejoin: "round", strokeMiterlimit: 4, strokeDasharray: "none", strokeOpacity: 1, }}>
-            <path d="M 8,12 L 37,12 L 37,26 L 8,26 L 8,12 Z M 8,26 L 14,29.5 L 31,29.5 L 37,26 M 14,29.5 L 14,31.5 L 31,31.5 L 31,29.5 M 14,31.5 C 14,31.5 14,34 14,34 C 14,34 11,34 11,34 C 11,34 11,36 11,36 C 11,36 12.5,38 12.5,38 L 32.5,38 C 32.5,38 34,36 34,36 C 34,36 34,34 34,34 C 34,34 31,34 31,34 C 31,34 31,31.5 31,31.5 M 12.5,38 L 12.5,40 L 32.5,40 L 32.5,38" style={{ strokeLinecap: "butt" }} />
-            <path d="M 8,12 C 8,12 8.5,4.5 14,4.5 C 19.5,4.5 22.5,7 22.5,7 C 22.5,7 25.5,4.5 31,4.5 C 36.5,4.5 37,12 37,12" style={{ strokeLinecap: "butt" }} />
+            <path d="M12.5 31h20v-3h-20v3zM10.5 12c0-3.31 2.69-6 6-6s6 2.69 6 6c0 1.54-.58 2.94-1.5 4h-9c-.92-1.06-1.5-2.46-1.5-4zM10 28h25v-3H10v3z" />
         </g>
     </svg>
 );
 export const WhiteKing = (props: SVGProps<SVGSVGElement>) => (
     <svg viewBox="0 0 45 45" {...props}>
         <g {...whitePieceStyles} style={{ opacity: 1, fill: "#fff", fillOpacity: 1, fillRule: "evenodd", stroke: "#000", strokeWidth: 1.5, strokeLinecap: "round", strokeLinejoin: "round", strokeMiterlimit: 4, strokeDasharray: "none", strokeOpacity: 1, }}>
-            <path d="M 22.5,11.63 C 22.5,11.63 22.5,11.63 22.5,11.63 C 22.5,11.63 22.5,11.63 22.5,11.63 C 20.12,11.63 18.25,13.5 18.25,15.88 C 18.25,18.25 20.12,20.13 22.5,20.13 C 24.88,20.13 26.75,18.25 26.75,15.88 C 26.75,13.5 24.88,11.63 22.5,11.63 Z M 22.5,22 C 19.5,22 19,23.5 19,23.5 L 19,26 C 19,26 20.5,26 22.5,26 C 24.5,26 26,26 26,26 L 26,23.5 C 26,23.5 25.5,22 22.5,22 Z M 20,28 L 20,31.5 C 20,31.5 20,31.5 20,31.5 C 20,31.5 20,31.5 20,31.5 C 20,32.33 20.67,33 21.5,33 L 23.5,33 C 24.33,33 25,32.33 25,31.5 L 25,28 Z M 17.5,34 L 17.5,35.5 L 27.5,35.5 L 27.5,34 L 17.5,34 Z" style={{ strokeLinecap: "butt", strokeOpacity: 1 }} />
-            <path d="M 22.5,26 C 22.5,26 22.5,28 22.5,28" style={{ fill: "none", stroke: "#000", strokeLinejoin: "miter" }} />
-            <path d="M 22.5,11.625 C 22.5,11.625 22.5,6 22.5,6" style={{ fill: "none", stroke: "#000", strokeLinecap: "butt", strokeLinejoin: "miter" }} />
-            <path d="M 20,8 L 25,8" style={{ fill: "none", stroke: "#000", strokeLinecap: "butt" }} />
+            <path d="M22.5 31h-9V29.5h9V31zm-4.5-1.5a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0zM12 28.5c0-4.67 3.33-8 8-8s8 3.33 8 8v3h-16v-3zM22.5 6h-3v3h-3v3h3v-3h3V6z" />
         </g>
     </svg>
 );
 export const BlackKing = (props: SVGProps<SVGSVGElement>) => (
     <svg viewBox="0 0 45 45" {...props}>
         <g {...blackPieceStyles} style={{ opacity: 1, fill: "#000", fillOpacity: 1, fillRule: "evenodd", stroke: "#fff", strokeWidth: 1.5, strokeLinecap: "round", strokeLinejoin: "round", strokeMiterlimit: 4, strokeDasharray: "none", strokeOpacity: 1, }}>
-            <path d="M 22.5,11.63 C 22.5,11.63 22.5,11.63 22.5,11.63 C 22.5,11.63 22.5,11.63 22.5,11.63 C 20.12,11.63 18.25,13.5 18.25,15.88 C 18.25,18.25 20.12,20.13 22.5,20.13 C 24.88,20.13 26.75,18.25 26.75,15.88 C 26.75,13.5 24.88,11.63 22.5,11.63 Z M 22.5,22 C 19.5,22 19,23.5 19,23.5 L 19,26 C 19,26 20.5,26 22.5,26 C 24.5,26 26,26 26,26 L 26,23.5 C 26,23.5 25.5,22 22.5,22 Z M 20,28 L 20,31.5 C 20,31.5 20,31.5 20,31.5 C 20,31.5 20,31.5 20,31.5 C 20,32.33 20.67,33 21.5,33 L 23.5,33 C 24.33,33 25,32.33 25,31.5 L 25,28 Z M 17.5,34 L 17.5,35.5 L 27.5,35.5 L 27.5,34 L 17.5,34 Z" style={{ strokeLinecap: "butt", strokeOpacity: 1 }} />
-            <path d="M 22.5,26 C 22.5,26 22.5,28 22.5,28" style={{ fill: "none", stroke: "#fff", strokeLinejoin: "miter" }} />
-            <path d="M 22.5,11.625 C 22.5,11.625 22.5,6 22.5,6" style={{ fill: "none", stroke: "#fff", strokeLinecap: "butt", strokeLinejoin: "miter" }} />
-            <path d="M 20,8 L 25,8" style={{ fill: "none", stroke: "#fff", strokeLinecap: "butt" }} />
+            <path d="M22.5 31h-9V29.5h9V31zm-4.5-1.5a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0zM12 28.5c0-4.67 3.33-8 8-8s8 3.33 8 8v3h-16v-3zM22.5 6h-3v3h-3v3h3v-3h3V6z" />
         </g>
     </svg>
 );
--- src/lib/get-piece-icon.tsx
+++ src/lib/get-piece-icon.tsx
@@ -1,29 +1,26 @@
 import { WhitePawn, BlackPawn, WhiteRook, BlackRook, WhiteKnight, BlackKnight, WhiteBishop, BlackBishop, WhiteQueen, BlackQueen, WhiteKing, BlackKing } from "@/components/icons/chess-pieces";
-
 import React from 'react';
 
 export const getPieceIcon = (type: string, color: string) => {
-    const isWhite = color === '#f8fafc' || color === '#e2e8f0'; // Corresponds to black_white and natural_purple light colors
+    const isWhite = color === '#f8fafc' || color === '#e2e8f0';
 
-    const style: React.CSSProperties = {
-        strokeLinecap: 'round',
-        strokeLinejoin: 'round',
-    };
-
-    const pieceProps = {
-        style: style,
-        fill: color,
-        stroke: color === '#0f172a' || color === '#18181b' ? '#f8fafc' : '#0f172a',
-        strokeWidth: 1.5,
+    const props = {
+        style: {
+            fill: color,
+            stroke: (color === '#0f172a' || color === '#18181b') ? '#f8fafc' : '#0f172a',
+            strokeWidth: 1.5,
+            strokeLinejoin: 'round',
+            strokeLinecap: 'round',
+        } as React.CSSProperties
     };
 
     switch (type.toLowerCase()) {
-        case 'p': return isWhite ? <WhitePawn {...pieceProps} /> : <BlackPawn {...pieceProps} />;
-        case 'r': return isWhite ? <WhiteRook {...pieceProps} /> : <BlackRook {...pieceProps} />;
-        case 'n': return isWhite ? <WhiteKnight {...pieceProps} /> : <BlackKnight {...pieceProps} />;
-        case 'b': return isWhite ? <WhiteBishop {...pieceProps} /> : <BlackBishop {...pieceProps} />;
-        case 'q': return isWhite ? <WhiteQueen {...pieceProps} /> : <BlackQueen {...pieceProps} />;
-        case 'k': return isWhite ? <WhiteKing {...pieceProps} /> : <BlackKing {...pieceProps} />;
+        case 'p': return isWhite ? <WhitePawn {...props} /> : <BlackPawn {...props} />;
+        case 'r': return isWhite ? <WhiteRook {...props} /> : <BlackRook {...props} />;
+        case 'n': return isWhite ? <WhiteKnight {...props} /> : <BlackKnight {...props} />;
+        case 'b': return isWhite ? <WhiteBishop {...props} /> : <BlackBishop {...props} />;
+        case 'q': return isWhite ? <WhiteQueen {...props} /> : <BlackQueen {...props} />;
+        case 'k': return isWhite ? <WhiteKing {...props} /> : <BlackKing {...props} />;
         default: return null;
     }
 }
