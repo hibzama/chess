ba54e6a840a43b4f428aa80d62d8060a2d65dcf8
Qrex Academy <jdilshan306@gmail.com>
2025-08-04 12:55:57 +0000

    Runtime Error
    Error: getDoc is not defined
    
    src/app/admin/events/manag

--- src/app/admin/events/manage/page.tsx
+++ src/app/admin/events/manage/page.tsx
@@ -2,7 +2,7 @@
 'use client'
 import { useState, useEffect } from 'react';
 import { db } from '@/lib/firebase';
-import { collection, onSnapshot, doc, setDoc, serverTimestamp, deleteDoc, query, where, getDocs, collectionGroup } from 'firebase/firestore';
+import { collection, onSnapshot, doc, setDoc, serverTimestamp, deleteDoc, query, where, getDocs, collectionGroup, getDoc } from 'firebase/firestore';
 import { Card, CardContent, CardHeader, CardTitle, CardDescription, CardFooter } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
